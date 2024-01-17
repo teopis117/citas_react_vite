4074d78cdee92977b856ca05d70169ee453c9b2f
Teopis117 <lalo_dimo_teopis_@hotmail.com>
2024-01-17 07:14:14 -0600

    No fue mucho trabajo pero honesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header()
     return (
      <>
      <h1 className="font-black text-5xl text-center md:w-1/2 mx-auto">
-        Seguimiento Pacientes{" "}
+        con github{" "}
         <span className="text-indigo-600">Veterinaria</span>
         </h1>
      </>
